36b696d929079eea73dd639ac34f02956c3f2455
vashudev.sharma <vashusharma30@gmail.com>
2025-06-01 15:37:35 +0530

    test: unknown amount of numbers and newline separators chore: apply rubocop fixes and update rubocop config

--- Gemfile
+++ Gemfile
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
-git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
-gem "rspec"
-gem "rubocop", require: false
-gem "simplecov", require: false, group: :test
+gem 'rspec'
+gem 'rubocop', require: false
+gem 'rubocop-rspec', require: false
+gem 'simplecov', require: false, group: :test
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,18 +1,27 @@
-require "string_calculator"
+require 'string_calculator'
 
 RSpec.describe StringCalculator do
-  describe "#add" do
-    it "returns 0 for an empty string" do
-      calculator = StringCalculator.new
-      expect(calculator.add("")).to eq(0)
+  subject(:calculator) { described_class.new }
+
+  describe '#add' do
+    it 'returns 0 for an empty string' do
+      expect(calculator.add('')).to eq(0)
     end
 
-    it 'returns the number when a single number is passed' do
-      expect(subject.add("1")).to eq(1)
+    it 'returns 1 for a single number' do
+      expect(calculator.add('1')).to eq(1)
     end
 
-    it 'returns the sum of two numbers separated by comma' do
-  		expect(subject.add("1,2")).to eq(3)
-		end
+    it 'returns sum for two numbers' do
+      expect(calculator.add('1,2')).to eq(3)
+    end
+
+    it 'returns sum for multiple numbers' do
+      expect(calculator.add('1,2,3,4')).to eq(10)
+    end
+
+    it 'supports newlines as delimiter' do
+      expect(calculator.add("1\n2,3")).to eq(6)
+    end
   end
 end
