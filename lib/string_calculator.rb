d1c646cb64eb6a5e518e83a36b5899f854d8da36
vashudev.sharma <vashusharma30@gmail.com>
2025-06-01 20:00:19 +0530

    refactor: improve StringCalculator readability with method decomposition

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,31 +3,47 @@ class StringCalculator
     @call_count = 0
   end
 
-  def add(numbers)
+  def add(input)
     @call_count += 1
-    return 0 if numbers.empty?
+    return 0 if input.empty?
 
-    if numbers.start_with?('//')
-      header, numbers = numbers.split("\n", 2)
-      delimiters = if header.include?('[')
-                     header.scan(/\[([^\]]+)\]/).flatten
-                   else
-                     [header[2..]]
-                   end
-      delimiter = Regexp.union(delimiters)
-    else
-      delimiter = /,|\n/
-    end
+    delimiter = extract_delimiter(input)
+    numbers_string = extract_numbers_string(input)
+    numbers = parse_numbers(numbers_string, delimiter)
 
-    nums = numbers.split(delimiter).map(&:to_i)
-    nums.reject! { |n| n > 1000 }
-    negatives = nums.select(&:negative?)
-    raise "negatives not allowed #{negatives.join(',')}" unless negatives.empty?
+    validate_no_negatives(numbers)
 
-    nums.sum
+    numbers.reject { |n| n > 1000 }.sum
   end
 
   def called_count
     @call_count
+  end
+
+  private
+
+  def extract_delimiter(input)
+    return /,|\n/ unless input.start_with?('//')
+
+    header = input.split("\n", 2).first
+    delimiters = if header.include?('[')
+                   header.scan(/\[([^\]]+)\]/).flatten
+                 else
+                   [header[2..]]
+                 end
+    Regexp.union(delimiters)
+  end
+
+  def extract_numbers_string(input)
+    input.start_with?('//') ? input.split("\n", 2).last : input
+  end
+
+  def parse_numbers(numbers_string, delimiter)
+    numbers_string.split(delimiter).map(&:to_i)
+  end
+
+  def validate_no_negatives(numbers)
+    negatives = numbers.select(&:negative?)
+    raise "negatives not allowed #{negatives.join(',')}" unless negatives.empty?
   end
 end
